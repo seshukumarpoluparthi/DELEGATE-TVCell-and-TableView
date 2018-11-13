c807c543beecf52e6bda656ade911ac8a9bd7492
seshukumar.poluparthi@gmail.com <seshukumar.poluparthi@gmail.com>
2018-11-13 18:33:16 +0530

    animate view

--- TabCellDeleagate/inspectableView.swift
+++ TabCellDeleagate/inspectableView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import QuartzCore
 
 @IBDesignable open class InsptView : UIView{
     
@@ -74,4 +75,42 @@ import UIKit
     }
     
     
+}
+
+// Call this Function only, access from any where in your project
+// Default values here
+private let animationDuration: TimeInterval = 1.0
+private let deleyTime: TimeInterval = 0
+private let springDamping: CGFloat = 0.25
+private let lowSpringDamping: CGFloat = 0.50
+private let springVelocity: CGFloat = 8.00
+
+extension InsptView {
+    
+    //MARK:- Default Animation here
+    public func AnimateView(){
+        provideAnimation(animationDuration: animationDuration, deleyTime: deleyTime, springDamping: springDamping, springVelocity: springVelocity)
+    }
+    
+    //MARK:- Custom Animation here
+    public func AnimateViewWithSpringDuration(_ name:UIView, animationDuration:TimeInterval, springDamping:CGFloat, springVelocity:CGFloat){
+        provideAnimation(animationDuration: animationDuration, deleyTime: deleyTime, springDamping: springDamping, springVelocity: springVelocity)
+    }
+    
+    //MARK:- Low Damping Custom Animation here
+    public func AnimateViewWithSpringDurationWithLowDamping(_ name:UIView, animationDuration:TimeInterval, springVelocity:CGFloat){
+        provideAnimation(animationDuration: animationDuration, deleyTime: deleyTime, springDamping: lowSpringDamping, springVelocity: springVelocity)
+    }
+    
+    private func provideAnimation(animationDuration:TimeInterval, deleyTime:TimeInterval, springDamping:CGFloat, springVelocity:CGFloat){
+        self.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        UIView.animate(withDuration: animationDuration,
+                       delay: deleyTime,
+                       usingSpringWithDamping: springDamping,
+                       initialSpringVelocity: springVelocity,
+                       options: .allowUserInteraction,
+                       animations: {
+                        self.transform = CGAffineTransform.identity
+        })
+    }
 }
